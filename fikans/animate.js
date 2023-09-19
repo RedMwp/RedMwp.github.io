58fc563c82cc1789939dcfe30003e3243b621c73
redmwp1@gmail.com <redmwp1@gmail.com>
2023-09-19 17:31:42 +0200

    something new

--- fikans/animate.js
+++ fikans/animate.js
@@ -2,18 +2,29 @@ $(document).ready(function(){
     //menu toggle show hide
     $("#menu , .exit-menu").on("click",function(){
         $(".main-menu").toggleClass("active-main-menu");
+        
     });
-    $("#to").hide()
-    //form
+    
+    //forms
+    //from
+    $(".secondary-nav").hide();
     $("#from").on("change",function(){
-      $("#to").show();
-      $("#from").hide();
-        $(".secondary-nav").toggleClass("secondary-nav-active");
-        gsap.to(".main-nav",{
-            y:-4,
-            background:"red",
-            duration:1,
-            ease:"smooth"
+        $(".secondary-nav").toggle(100);
+    })
+    //hide the nav when user clicks the from form
+    $("#from").on("click",function(){
+        $(".menu").css({
+            opacity:0,
         })
     })
+  //action on the to form
+  $("#to").on("change",function(){
+      //something happens on change tob form
+      $(".secondary-nav").toggle(100);
+      ///shows the hidden menu again
+      gsap.to(".menu",{
+          opacity:1,
+          duration:1,
+      })
+  })
 });
